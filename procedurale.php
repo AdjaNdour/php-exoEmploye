af342e99515eb34b436404749e8572a55c9f96ce
AdjaNdour <adjacourandour4@gmail.com>
2026-06-30 22:05:14 +0000

    initialisation des categories et produits

---
+++ procedurale.php
@@ -0,0 +1,33 @@
+<?php
+
+//1 initialisation des categories et produits
+
+$categories = [
+    0 =>[
+        "code" => "cat0",
+        "nom" => "categorie0",
+        "produits" => [
+                0 => [
+                    "nom" => "prod1",
+                    "reference" => "ref1",
+                    "prix" => 3000,
+                    "quantite" => 5 
+                    ],
+                1 => [
+                    "nom" => "prod2",
+                    "reference" => "ref2",
+                    "prix" => 2000,
+                    "quantite" => 3 
+                    ]
+        ]
+    ],
+    1 =>  [
+        "code" => "cat1",
+        "nom" => "categorie1",
+        "produits" => []
+    ]
+];
+
+
+
+?>
